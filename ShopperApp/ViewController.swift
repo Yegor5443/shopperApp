26956e4ea1444a97f8d38494e1b9b9d77eb07d77
Егор Меховской <yehor.meho@Egors-MacBook-Pro.local>
2019-03-12 18:08:28 +0200

    first commit

---
+++ ShopperApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopperApp
+//
+//  Created by Егор Меховской  on 3/12/19.
+//  Copyright © 2019 Yehor Mekhovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
